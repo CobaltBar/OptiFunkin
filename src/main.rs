fa56d57d3b844de1dc95d84c122176a1e62c021b
Cobalt Bar <79053181+CobaltBar@users.noreply.github.com>
2024-09-08 16:28:26 -0700

    some progress

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use std::path::PathBuf;
+
 use clap::Parser;
+use max_rects::{
+    bucket::Bucket, calculate_packed_percentage, max_rects::MaxRects, packing_box::PackingBox,
+};
+use rayon::prelude::*;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct CLI {
     /// Files or Folders to optimize and repack
-    files: Vec<String>,
+    files: Vec<PathBuf>,
 
     /// Recursively go through directories for files
     #[arg(short, long)]
@@ -17,4 +23,20 @@ struct CLI {
 
 fn main() {
     let cli = CLI::parse();
+
+    let mut files: Vec<&PathBuf> = cli.files.par_iter().filter(|&f| f.exists()).collect();
+
+    let nonexistent = cli
+        .files
+        .par_iter()
+        .filter(|&f| !f.exists())
+        .map(|f| f.to_str().unwrap())
+        .collect::<Vec<&str>>()
+        .join("', '");
+
+    if nonexistent.len() != 0 {
+        println!("WARN: The following paths do not exist: '{}'", nonexistent);
+    }
+
+    println!("{:?}", files);
 }
